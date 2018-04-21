81b46d0ed305ae8ce86f7e5b6e45db32b04e20a3
Frank Matranga <thefrankmatranga@gmail.com>
2018-04-21 17:35:56 -0400

    Basic middleware

---
+++ index.js
@@ -0,0 +1,4 @@
+const server = require('./server');
+
+const port = process.env.PORT || 3000;
+server.listen(port, () => console.log(`API server started on ${port}`));
--- server.js
+++ server.js
@@ -1,8 +1,23 @@
 const Koa = require('koa');
+const Body = require('koa-body');
+const Router = require('koa-router');
+const Logger = require('koa-logger');
+const Helmet = require('koa-helmet');
+const respond = require('koa-respond');
+
 const app = new Koa();
+const router = new Router();
+
+app.use(Helmet());
+
+if (process.env.NODE_ENV === 'development') {
+  app.use(Logger());
+}
+app.use(Body());
+app.use(respond());
 
 app.use(async ctx => {
   ctx.body = 'Hello World';
 });
 
-app.listen(3000);
+module.exports = app;
