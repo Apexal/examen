9a0296a8c32888803ac73dd97016218a9dc8964e
Frank Matranga <thefrankmatranga@gmail.com>
2018-05-23 14:28:08 -0400

    Examen view

--- controllers/examen.js
+++ controllers/examen.js
@@ -1,9 +1,25 @@
+/* GET one particular examen by ID [maybe date??] */
+async function view_examen(ctx) {
+  // Find examen
+  let examen;
+  try {
+    // HOW COOL IS THIS
+    examen = ctx.state.examen = await ctx.db.Examen.findById(ctx.params.id);
+    
+  } catch (e) {
+    return ctx.throw(404, 'Examen Not Found');
+  }
+
+  ctx.state.title = examen.title;
+  await ctx.render('examen/examen');
+}
+
+/* GET a list of all posted examens */
 async function view_archive(ctx) {
   ctx.state.title = 'Archive';
-
   ctx.state.examens = await ctx.db.Examen.find();
 
   await ctx.render('examen/archive');
 }
 
-module.exports = { view_archive };
+module.exports = { view_examen, view_archive };
--- routes/examen.js
+++ routes/examen.js
@@ -4,5 +4,6 @@ const router = new Router();
 const Ctrl = require('../controllers/examen');
 
 router.get('/archive', Ctrl.view_archive);
+router.get('/:id', Ctrl.view_examen);
 
 module.exports = router.routes();
