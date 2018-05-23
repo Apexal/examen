c7fd947daf31270890e273aec35da149130835a9
Frank Matranga <thefrankmatranga@gmail.com>
2018-05-23 12:50:44 -0400

    Examen schema update - text -> introduction

--- db/schemas/examen.js
+++ db/schemas/examen.js
@@ -3,7 +3,7 @@ const Schema = mongoose.Schema;
 
 const schema = new Schema({
   title: { type: String, required: true, maxlength: 200 },
-  text: { type: String, required: true, maxlength: 2000 },
+  introduction: { type: String, required: true, maxlength: 10000 },
   questions: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Question' }],
   dateAdded: { type: Date, required: true }
 });
