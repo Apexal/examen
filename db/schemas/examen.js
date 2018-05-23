060f5103b2be5c9072f8d721181f184f689bbd44
Frank Matranga <thefrankmatranga@gmail.com>
2018-05-23 09:28:05 -0400

    Preliminary examen schema

--- db/schemas/examen.js
+++ db/schemas/examen.js
@@ -2,10 +2,10 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const schema = new Schema({
-  _examen: { type: mongoose.Schema.Types.ObjectId, ref: 'Examen' },
   title: { type: String, required: true, maxlength: 200 },
   text: { type: String, required: true, maxlength: 2000 },
+  questions: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Question' }],
   dateAdded: { type: Date, required: true }
 });
 
-module.exports = { name: 'Question', schema };
+module.exports = { name: 'Examen', schema };
