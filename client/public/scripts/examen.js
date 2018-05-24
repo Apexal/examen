f9099c8f2289b006fc1f0477576b2dc44e4ea237
Frank Matranga <thefrankmatranga@gmail.com>
2018-05-24 01:03:56 -0400

    Track active tab CSS examen page

--- client/public/scripts/examen.js
+++ client/public/scripts/examen.js
@@ -2,7 +2,8 @@ document.querySelectorAll('#examen-breadcrumb li a').forEach(link => {
   const target = document.getElementById(link.dataset.target);
 
   link.onclick = function (event) {
-    document.querySelectorAll('.prompts .is-active').forEach(link => link.classList.remove('is-active'));
+    document.querySelectorAll('.prompts .is-active, #examen-breadcrumb li.is-active').forEach(link => link.classList.remove('is-active'));
     target.classList.add('is-active');
+    link.parentElement.classList.add('is-active');
   }
 });
