b587c9bbc2cf38bafe3240dc79d82a00612fa410
Frank Matranga <thefrankmatranga@gmail.com>
2018-05-25 00:55:48 -0400

    RECORD AUDIO PER PROMPT - cannot submit them yet

--- client/public/scripts/new.js
+++ client/public/scripts/new.js
@@ -1,23 +1,60 @@
 const new_examen_app = new Vue({
+  name: 'new-examen',
   el: '#new-examen',
   data: {
+    stream: null,
+    title: '',
+    backingTrack: null,
     introduction: '',
-    prompts: [{
-      text: '',
-      recording: false,
-      delay: 30,
-      chunks: []
-    }]
+    prompts: []
+  },
+  created: async function () {
+    this.stream = await navigator.mediaDevices.getUserMedia({
+      audio: true,
+      video: false
+    });
   },
   methods: {
     addPrompt: function (event) {
-      this.prompts.push({
+      const prompt = {
         text: '',
+        recorder: new MediaRecorder(this.stream),
         recording: false,
         delay: 30,
-        chunks: []
+        chunks: [],
+        src: null
+      };
+
+      prompt.recorder.addEventListener("dataavailable", event => {
+        prompt.chunks.push(event.data);
       });
+
+      prompt.recorder.onstop = () => {
+        const audioBlob = new Blob(prompt.chunks);
+        const audioUrl = URL.createObjectURL(audioBlob);
+        prompt.src = audioUrl;
+        alert('done!');
+      }
+
+      this.prompts.push(prompt);
       setTimeout(() => setupEditor(document.getElementById('prompt-' + (this.prompts.length - 1) + '-editor')), 100);
+    },
+    toggleRecording: function (index) {
+      if (this.prompts[index].recording) {
+        this.stopRecording(index);
+      } else {
+        this.startRecording(index);
+      }
+
+      this.prompts[index].recording = !this.prompts[index].recording;
+    },
+    startRecording: function (index) {
+      this.prompts[index].chunks = [];
+      this.prompts[index].src = null;
+      this.prompts[index].recorder.start();
+    },
+    stopRecording: function (index) {
+      this.prompts[index].recorder.stop();
     }
   }
 });
@@ -51,27 +88,3 @@ function setupEditor(editor) {
 
 document.querySelectorAll('.editor.prompt-editor').forEach(setupEditor);
 setupEditor(document.querySelector('.editor.introduction-editor'));
-
-
-// -------------------------------------------------------------------------
-
-document.querySelectorAll('.audio input.recorder').forEach(function(recorder) {
-  const player = document.getElementById(recorder.dataset.target);
-  const chunks = [];
-
-  const handleSuccess = function(stream) {
-    const mediaRecorder = new MediaRecorder(stream);
-
-  };
-
-  navigator.mediaDevices.getUserMedia({ audio: true, video: false })
-      .then(handleSuccess);
-
-
-  recorder.addEventListener('change', function(e) {
-    let file = e.target.files[0];
-    // Do something with the audio file.
-    player.src =  URL.createObjectURL(file);
-  });
-
-});
