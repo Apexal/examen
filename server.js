52b0d61d00e66cbbcbff4b3e540be7ff996e7282
Frank Matranga <thefrankmatranga@gmail.com>
2018-05-26 15:35:15 -0400

    Cleaned up code and added comments

--- server.js
+++ server.js
@@ -11,8 +11,6 @@ const session = require('koa-session');
 const db = require('./db');
 const static = require('koa-static');
 const passport = require('./auth');
-// TODO: find proper flash
-
 
 const config = require('config');
 
@@ -30,6 +28,7 @@ app.use(
   })
 );
 
+/* Serve static files (CSS, JS, audio, etc.) */
 app.use(static('client/public'));
 
 /* Session setup */
@@ -46,22 +45,30 @@ app.context.helpers = require('./helpers');
 
 /* Locals */
 app.use(async (ctx, next) => {
+  // Create flash session object if does not exist yet (first request)
   if (ctx.session.flash === undefined) {
     ctx.session.flash = {};
   }
-  ctx.request.flash = (type, msg) => { // Inject flash function into request
+
+  // Inject flash function into request
+  ctx.request.flash = (type, msg) => {
     ctx.session.flash[type] = msg;
   };
 
+  // Empty the flash but before that pass it to the state to use in views
   ctx.state.flash = ctx.session.flash;
   ctx.session.flash = {};
 
   ctx.state.path = ctx.request.url;
+
+  // To use in views
   ctx.state.helpers = ctx.helpers;
   ctx.state.loggedIn = ctx.isAuthenticated();
   ctx.state.moment = require('moment');
+
   await next();
 
+  // Keep flash on redirect so it is not lost
   if (ctx.status === 302 && ctx.session && !(ctx.session.flash)) {
     ctx.session.flash = ctx.state.flash;
   }
@@ -108,12 +115,14 @@ require('./routes')(router);
 app.use(router.routes());
 app.use(router.allowedMethods());
 
+/* Bring user to Google login asking for permission */
 router.get('/auth/google',
   passport.authenticate('google', {
-    scope: ['profile', 'email'] /*, session: true */
+    scope: ['profile', 'email']
   })
 );
 
+/* Actually login once returned from the Google login site */
 router.get('/auth/google/callback',
   passport.authenticate('google', {
     successRedirect: '/',
@@ -121,6 +130,7 @@ router.get('/auth/google/callback',
   })
 );
 
+/* Logout with Passport and redirect home with flash */
 router.get('/logout', ctx => {
   ctx.logout();
   ctx.request.flash('success', 'Successfully logged out.');
