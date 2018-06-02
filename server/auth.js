a8fe03c9f472b902fb4c70441063de9afbbd79b9
Frank Matranga <thefrankmatranga@gmail.com>
2018-06-02 00:05:30 -0400

    Auto determine school on user first join (closes #21) - Also fix regex bug

--- server/auth.js
+++ server/auth.js
@@ -1,11 +1,14 @@
 const config = require('config');
 const passport = require('koa-passport');
 
-const User = require('../db').User;
+const {
+  School,
+  User
+} = require('../db');
 
 // This regex matches emails in the student format
 // username + 2 digit graduation year + '@regis.org'
-const studentEmailRegex = /^[a-zA-Z]+\d{2}@regis.org$/g;
+const studentEmailRegex = /^[a-zA-Z]+\d{2}@regis.org$/;
 
 /* Saves the user id to the session */
 passport.serializeUser((user, done) => {
@@ -20,9 +23,6 @@ passport.deserializeUser((id, done) => {
 const GoogleStrategy = require('passport-google-auth').Strategy;
 passport.use(new GoogleStrategy(config.get('auth.google'),
   async (token, tokenSecret, profile, done) => {
-    // Ensure only Regis google accounts can be used
-    if (profile.domain !== 'regis.org') return done(new Error('Must use a Regis email.'));
-
     // First email
     const email = profile.emails[0].value;
 
@@ -38,9 +38,23 @@ passport.use(new GoogleStrategy(config.get('auth.google'),
     }
 
     if (!user) {
+      // Determine school by domain
+      let school;
+
+      try {
+        school = await School.findOne({
+          domain: profile.domain
+        });
+
+        if (!school) throw new Error(`Failed to find school with domain "${profile.domain}"`);
+      } catch (e) {
+        return done(e);
+      }
+
       // New user
       user = new User({
         _google_id: profile.id, // Users have an _id and a separate _google_id
+        _school: school,
         name: {
           first: profile.name.givenName,
           last: profile.name.familyName
