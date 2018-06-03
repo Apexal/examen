6c1a22c16c375fa284d571c3495668e7b5d3e654
Frank Matranga <thefrankmatranga@gmail.com>
2018-06-03 14:57:40 -0400

    Proper page auth

--- server/routes/examen.js
+++ server/routes/examen.js
@@ -4,24 +4,33 @@ const router = new Router();
 const Ctrl = require('../controllers/examen');
 
 /* Allows only teachers to use theses routes */
-router.all(['/new', '/:id/remove', '/submissions'], async (ctx, next) => {
+const requireLogin = async (ctx, next) => {
+  if (ctx.isAuthenticated()) {
+    await next();
+  } else {
+    ctx.request.flash('danger', 'You must be logged in to view that page.');
+    await ctx.redirect(ctx.router.url('archive'));
+  }
+};
+
+const requireStaff = async (ctx, next) => {
   if (ctx.isAuthenticated() && !ctx.state.user.isStudent) {
     await next();
   } else {
     ctx.request.flash('danger', 'You must be logged in as a teacher to view that page.');
     await ctx.redirect(ctx.router.url('archive'));
   }
-});
+};
 
 router.get('/today', Ctrl.redirect_today);
 
-router.get('/new', Ctrl.view_new_examen);
-router.post('/new', Ctrl.save_new_examen);
+router.get('/new', requireLogin, Ctrl.view_new_examen);
+router.post('/new', requireLogin, Ctrl.save_new_examen);
 
 router.get('archive', '/archive', Ctrl.view_archive);
-router.get('submissions', '/submissions', Ctrl.view_submissions);
+router.get('submissions', '/submissions', requireStaff, Ctrl.view_submissions);
 
 router.get('examen', '/:id', Ctrl.view_examen);
-router.post('/:id/remove', Ctrl.remove_examen);
+router.post('/:id/remove', requireStaff, Ctrl.remove_examen);
 
 module.exports = router.routes();
